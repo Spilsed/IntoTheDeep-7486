7de88d9131657ee6e236aa080dadbd85e56d7788
Retinova <93678295+Spilsed@users.noreply.github.com>
2024-11-25 15:33:29 -0500

    Add new robot parts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supers/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supers/Robot.kt
@@ -25,6 +25,8 @@ class Robot (opMode: OpMode, resetEncoders: Boolean = true) {
     var hand: ServoHand
     var wrist: Wrist
     var linearActuator: LinearSlide
+    var rotationalArm: RotationalArm
+    var lift: Lift
 
     var odo: GoBildaPinpointDriver
     var octoQuad: OctoQuad
@@ -62,6 +64,8 @@ class Robot (opMode: OpMode, resetEncoders: Boolean = true) {
         hand = ServoHand(hardwareMap.get(Servo::class.java, "hand"), 0.0, 1.0)
         wrist = Wrist(hardwareMap.get(Servo::class.java, "wrist1"), hardwareMap.get(Servo::class.java, "wrist2"))
         linearActuator = LinearSlide(hardwareMap.get(DcMotor::class.java, "la"), 537.7, 5.2)
+        rotationalArm = RotationalArm(hardwareMap.get(DcMotor::class.java, "arm1"), hardwareMap.get(DcMotor::class.java, "arm2"), 0, 10)
+        lift = Lift(hardwareMap.get(DcMotor::class.java, "liftm"), hardwareMap.get(Servo::class.java, "lifts"), 537.7, 1.0, 100, 0, 0.5, 0.0)
 
         odo = hardwareMap.get(GoBildaPinpointDriver::class.java, "odo")
         odo.setOffsets(-174.625, 142.38478)
