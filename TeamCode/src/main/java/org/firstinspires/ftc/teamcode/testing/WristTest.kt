ab5fc29f37edd2736bb12e6c46c32265fba4f5b7
Spilsed <Spilsed>
2025-01-18 12:22:53 -0500

    Manual wrist override

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WristTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WristTest.kt
@@ -17,14 +17,14 @@ class WristTest: LinearOpMode() {
         waitForStart()
 
         while (opModeIsActive()) {
-            telemetry.addData("LWrist", r.wrist.lServo.position)
-            telemetry.addData("RWrist", r.wrist.rServo.position)
+            //telemetry.addData("LWrist", r.wrist.lServo.position)
+            //telemetry.addData("RWrist", r.wrist.rServo.position)
             telemetry.update()
 
             r.wrist.turn(gamepad1.left_stick_y.toDouble() / 100)
             r.wrist.twist(gamepad1.left_stick_x.toDouble() / 100)
             // This is what makes the servos actually move this needs to be where ever the wrist is used
-            r.wrist.updatePositions()
+            //r.wrist.updatePositions()
         }
     }
 }
