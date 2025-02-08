c4b3045680c167be1d4fdf1fa11beac5cc5cf474
Retinova <93678295+Spilsed@users.noreply.github.com>
2025-02-08 10:03:26 -0500

    last minute changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LiftTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LiftTest.kt
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.testing
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
-import org.firstinspires.ftc.teamcode.supers.
+import org.firstinspires.ftc.teamcode.supers.Robot
 
 @TeleOp(name = "Lift Test")
 class LiftTest: LinearOpMode() {
