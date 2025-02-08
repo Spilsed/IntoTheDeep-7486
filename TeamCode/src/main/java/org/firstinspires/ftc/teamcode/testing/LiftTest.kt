66ca5aa7b2285a956f2f59d3fa9978a1256e3954
Retinova <93678295+Spilsed@users.noreply.github.com>
2025-02-08 12:32:29 -0500

    Lift code at comp. v1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/LiftTest.kt
@@ -0,0 +1,84 @@
+/*package org.firstinspires.ftc.teamcode.testing
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.CRServo
+import com.qualcomm.robotcore.hardware.DcMotor
+import org.firstinspires.ftc.teamcode.supers.Robot
+
+private fun CRServo.update() {
+    ("NOT DONE")
+}
+
+@TeleOp(name = "Lift Test")
+class LiftTest: LinearOpMode() {
+    lateinit var r: Robot
+
+    override fun runOpMode() {
+        r = Robot(this)
+
+        waitForStart()
+
+        //r.lift.motor.mode = DcMotor.RunMode.RUN_USING_ENCODER
+        while (opModeIsActive()) {
+//            if (gamepad1.a) {
+//                r.lift.expand(0.1)
+//            } else if (gamepad1.b) {
+//                r.lift.expand(-0.2)
+//            }
+
+
+            r.lift.motor.power = gamepad1.right_stick_y.toDouble()
+            r.lift.servo.power = gamepad2.right_stick_x.toDouble()
+            //r.lift.servo.update()
+            /*
+            while (gamepad1.right_bumper) {
+                r.lift.servo.power = 1.0
+            }
+            while (gamepad1.left_bumper) {
+                r.lift.servo.power = -1.0
+            }
+
+            else {
+                r.lift.servo.power = 0.0
+            }
+            */
+            if (gamepad1.right_bumper) {
+                r.lift.servo.power = 1.0
+            }
+            else if (gamepad1.left_bumper) {
+                r.lift.servo.power = -1.0
+            }
+            else {
+                r.lift.servo.power = 0.0
+            }
+
+        }
+    }
+}*/
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.CRServo
+
+class CRServoControl : OpMode() {
+
+    private lateinit var crServo: CRServo
+
+    override fun init() {
+        crServo = hardwareMap.get(CRServo::class.java, "crServo")
+    }
+    override fun loop() {
+        when {
+            gamepad1.right_bumper -> {
+                crServo.power = 1.0
+            }
+            gamepad1.left_bumper -> {
+                crServo.power = -1.0
+            }
+            else -> {
+                crServo.power = 0.0
+            }
+        }
+    }
+}
