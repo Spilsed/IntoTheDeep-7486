80451a219704e2a43465b6be3d2f80b6d4b4c49e
Spilsed <Spilsed>
2025-02-16 17:50:38 -0500

    ThreeDeadWheelLocalizer dead wheel locations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunner/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunner/ThreeDeadWheelLocalizer.java
@@ -14,7 +14,6 @@ import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
 import com.acmerobotics.roadrunner.ftc.PositionVelocityPair;
 import com.acmerobotics.roadrunner.ftc.RawEncoder;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.roadRunner.messages.ThreeDeadWheelInputsMessage;
@@ -22,9 +21,9 @@ import org.firstinspires.ftc.teamcode.roadRunner.messages.ThreeDeadWheelInputsMe
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks = 0.0; // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 1.0; // y position of the second parallel encoder (in tick units)
-        public double perpXTicks = 0.0; // x position of the perpendicular encoder (in tick units)
+        public double par0YTicks = -1682.5; //-1425.51963583; // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = 1682.5; //1425.51963583; // y position of the second parallel encoder (in tick units)
+        public double perpXTicks = -2842.5540524; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
@@ -46,7 +45,7 @@ public final class ThreeDeadWheelLocalizer implements Localizer {
         perp = new OverflowEncoder(new RawEncoder(hardwareMap.get(DcMotorEx.class, "la")));
 
         // TODO: reverse encoder directions if needed
-        par0.setDirection(DcMotorSimple.Direction.REVERSE);
+        // par0.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.inPerTick = inPerTick;
 
