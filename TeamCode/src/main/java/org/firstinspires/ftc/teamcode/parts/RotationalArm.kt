fefcde249fa05a26ea40a94f4c1814e4b0a6c3a8
Retinova <93678295+Spilsed@users.noreply.github.com>
2025-02-21 15:33:04 -0500

    Set power for autonomous motion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/RotationalArm.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/parts/RotationalArm.kt
@@ -19,8 +19,9 @@ class RotationalArm(val motor1: DcMotor, val motor2: DcMotor, var min: Int, val 
         motor2.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
 
         if (auto) {
-            motor1.mode = DcMotor.RunMode.RUN_TO_POSITION
-            motor2.mode = DcMotor.RunMode.RUN_TO_POSITION
+            motors.targetPosition = motor1.currentPosition
+            motors.mode = DcMotor.RunMode.RUN_TO_POSITION
+            motors.power = 0.5
         } else {
             motor1.mode = DcMotor.RunMode.RUN_USING_ENCODER
             motor2.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
