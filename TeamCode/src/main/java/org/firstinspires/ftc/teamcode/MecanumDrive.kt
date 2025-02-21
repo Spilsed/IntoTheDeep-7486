7e6feef293907e53cc0ac5d91133ab66c533f4b1
Retinova <93678295+Spilsed@users.noreply.github.com>
2025-02-21 13:49:41 -0500

    Disable duplicate OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.kt
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 
-@TeleOp(name = "Basic")
+@TeleOp(name = "MecanumDrive")
+@Disabled
 class MecanumDrive : LinearOpMode() {
 
     private lateinit var leftFrontMotor: DcMotor
