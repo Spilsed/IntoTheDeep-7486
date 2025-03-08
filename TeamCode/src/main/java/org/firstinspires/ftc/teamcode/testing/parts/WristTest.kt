262efb5fd50d94ce882f76fbeac6fdffc88945a4
Retinova <93678295+Spilsed@users.noreply.github.com>
2025-03-08 13:04:30 -0500

    Change from class to raw powers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/parts/WristTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/parts/WristTest.kt
@@ -14,8 +14,18 @@ class WristTest: LinearOpMode() {
         waitForStart()
 
         while (opModeIsActive()) {
-            r.wrist.turn(gamepad1.left_stick_y.toDouble() / 4)
-            r.wrist.twist(gamepad1.left_stick_x.toDouble() / 4)
+            // r.wrist.turn(gamepad1.left_stick_y.toDouble() / 4)
+            // r.wrist.twist(gamepad1.left_stick_x.toDouble() / 4)
+            r.wrist.lServo.power = gamepad1.left_stick_x.toDouble()
+            r.wrist.rServo.power = gamepad1.right_stick_x.toDouble()
+
+            if (gamepad1.a) {
+                r.wrist.lServo.power = -0.5
+                r.wrist.rServo.power = 0.5
+            } else if (gamepad1.b) {
+                r.wrist.lServo.power = 0.5
+                r.wrist.rServo.power = -0.5
+            }
 
             telemetry.addData("LWrist", r.wrist.lServo.power)
             telemetry.addData("RWrist", r.wrist.rServo.power)
